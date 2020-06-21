900ba928a8146481a1010464a05d8f95587062c2
Joe Biggins <jjbiggins@joebiggins.io>
2020-06-20 23:28:38 -0500

    created remote git repo

---
+++ ch1/GoogStock/goog.py
@@ -0,0 +1,7 @@
+# File              : goog.py
+# Author            : Joe Biggins <jjbiggins@joebiggins.io>
+# Date              : 20.06.2020
+# Last Modified Date: 20.06.2020
+
+import numpy as np
+mport
