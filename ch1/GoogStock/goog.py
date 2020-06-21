f298533085eae02d2a6296d77208c490cd3efe9b
Joe Biggins <jjbiggins@joebiggins.io>
2020-06-21 01:54:57 -0500

    played with python configurations using goog.py as test program

--- ch1/GoogStock/goog.py
+++ ch1/GoogStock/goog.py
@@ -1,7 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # File              : goog.py
 # Author            : Joe Biggins <jjbiggins@joebiggins.io>
-# Date              : 20.06.2020
-# Last Modified Date: 20.06.2020
+# Date              : 21.06.2020
+# Last Modified Date: 21.06.2020
 
+import sys
+import os
+import sys
 import numpy as np
-mport
+import pandas as pd
+
+import pandas_datareader as pdr
+import pandas_datareader.data as web
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def readData():
+    df = web.DataReader('GOOG', 'quandl', start='2009-03-14', end='2020-06-01', api_key='9UfDvyheeFPxTJxovpBz')
+    print(df.tail())
+
+
+def volatilityAnalytics():
+
+    goog = web.DataReader('GOOG', 'quandl', start='2009-03-14', end='2020-06-01', api_key='9UfDvyheeFPxTJxovpBz')
+    goog['Log_Ret'] = (np.log(goog['Close'] / goog['Close'].shift(1)))
+    goog['Volatility'] = goog['Log_Ret'].rolling( window=252).std() * np.sqrt(252)
+    print(goog['Volatility'])
+
+    goog[['Close', 'Volatility']].plot(subplots=True, color='blue', figsize=(8,6))
+    plt.show()
+
+if __name__ == "__main__":
+    #readData()
+    volatilityAnalytics()
---
+++ ch1/GoogStock/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : test.py
+# Author            : Joe Biggins <jjbiggins@joebiggins.io>
+# Date              : 20.06.2020
+# Last Modified Date: 20.06.2020
+# Last Modified By  : Joe Biggins <jjbiggins@joebiggins.io>
+
